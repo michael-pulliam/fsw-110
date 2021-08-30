b7b94d1f1a946a9fa394422bc90e4944d33bf019
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2021-08-30 11:10:11 -0500

    adding week5 modifying Elements value assignment

--- week-4/CalculatorJS/index2.js
+++ week-4/CalculatorJS/index2.js
@@ -14,3 +14,24 @@ function calc() {
     document.getElementById("mult").value = mult;
     
 }
+
+/*
+function addition (num1, num2){
+    return(num1+num2);
+}
+function subtraction (num1, num2){
+    return(num1-num2);
+}
+function multiplication (num1, num2){
+    return(num1*num2);
+}​
+document.getElementById("add").addEventListener("click", function(){
+    document.getElementById("answer").textContent = addition(Number(document.getElementById("firstnum").value), Number(document.getElementById("secondnum").value));
+})
+document.getElementById("sub").addEventListener("click", function(){
+    document.getElementById("answer").textContent = subtraction(Number(document.getElementById("firstnum2").value), Number(document.getElementById("secondnum2").value));
+})
+document.getElementById("mult").addEventListener("click", function(){
+    document.getElementById("answer").textContent = multiplication(Number(document.getElementById("firstnum3").value), Number(document.getElementById("secondnum3").value));
+})
+​*/
---
+++ week-5/modifyElementsValue/index.js
@@ -0,0 +1,9 @@
+
+function myFunction(){
+    var input = document.getElementById("name").value
+    var last = document.getElementById("last").value
+    var age = document.getElementById("age").value
+    alert(`${input} ${last} ${age}`);
+    document.form.reset();
+    
+}
